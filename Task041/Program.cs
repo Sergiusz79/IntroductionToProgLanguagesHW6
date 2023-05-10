42885934cd4376b3d8888743de7d6e8725856851
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-05-10 22:10:48 +0200

    Task041

--- Task041/Program.cs
+++ Task041/Program.cs
@@ -4,4 +4,39 @@
 //           1, -7, 567, 89, 223 -> 3
 
 
+void EnterNumbers(int[] arr)
+{
+int num;
+Console.WriteLine("Please enter numbers: ");
 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        while (!int.TryParse(Console.ReadLine(), out num))
+        {
+        Console.WriteLine("This is not number! Please enter number!");
+        }
+    arr[i] = num;
+    }
+}
+
+void CheckForPositive(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 0) count++;
+    }
+    Console.WriteLine($"{count} entered positive numbers!");
+}
+
+
+
+void Task041()
+{
+int[] arr = new int[8];
+EnterNumbers(arr);
+CheckForPositive(arr);
+}
+
+
+Task041();
